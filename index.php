c01a505d3ecf0910534c8fb1dd8bca2baa292f28
Fesposito9197 <kekkoesp8@gmail.com>
2023-01-10 16:53:26 +0100

    aggiunge liste separate products

--- index.php
+++ index.php
@@ -9,11 +9,16 @@ $dog = new Categorie("cane");
 
 $cat = new Categorie("gatto");
 
-$products = [
+
+$products_food = [
     new Food([$dog],"felix","crocchette", 10, "product image", "carne rossa"),
     new Food([$cat], "purina", "bocconcini", 7, "product image", "pesce"),
+];
+$products_kennels = [
     new Kennels([$dog], "baldiflex", "cuccia in legno", 100, "product image", 120),
     new Kennels([$cat], "pawhut", "cuccia di vimini", 110, "product image", 50 ),
+];
+$products_toy = [
     new Toy([$dog],"trixie", "corda da gioco" , 80, "product image", "stoffa"),
     new Toy([$cat],"dadypet","graffiatoio" ,60 ,"product image" , "plastica"),
 ];
@@ -29,7 +34,7 @@ $products = [
     </head>
     <body>
         <ul>
-            <?php foreach($products as $product) { ?>
+            <?php foreach($products_food as $product) { ?>
             <li>
                 <?php foreach($product->categories as $categorie) { ?>
                 <h3>Prodotto per: <?php echo ucfirst($categorie->name) ?>  </h3>
@@ -40,7 +45,37 @@ $products = [
                     <li><?php echo ucfirst($product->type) ?></li>
                     <li><?php echo ucfirst($product->price) ?> euro</li>
                     <li><?php echo ucfirst($product->protein) ?></li>
+                </ul>
+            </li>
+            <?php } ?>
+        </ul>
+        <ul>
+            <?php foreach($products_kennels as $product) { ?>
+            <li>
+                <?php foreach($product->categories as $categorie) { ?>
+                <h3>Prodotto per: <?php echo ucfirst($categorie->name) ?>  </h3>
+                <?php } ?>
+                <ul>
+                    <li><?php echo ucfirst($product->image) ?></li>
+                    <li><?php echo ucfirst($product->name) ?></li>
+                    <li><?php echo ucfirst($product->type) ?></li>
+                    <li><?php echo ucfirst($product->price) ?> euro</li>
                     <li><?php echo ucfirst($product->size) ?> cm</li>
+                </ul>
+            </li>
+            <?php } ?>
+        </ul>
+        <ul>
+            <?php foreach($products_toy as $product) { ?>
+            <li>
+                <?php foreach($product->categories as $categorie) { ?>
+                <h3>Prodotto per: <?php echo ucfirst($categorie->name) ?>  </h3>
+                <?php } ?>
+                <ul>
+                    <li><?php echo ucfirst($product->image) ?></li>
+                    <li><?php echo ucfirst($product->name) ?></li>
+                    <li><?php echo ucfirst($product->type) ?></li>
+                    <li><?php echo ucfirst($product->price) ?> euro</li>
                     <li><?php echo ucfirst($product->material) ?></li>
                 </ul>
             </li>
